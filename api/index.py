1b6604b9e91a87d89e64839a0bd5d601be887fda
Tanishka Agrawal <tanishkaagrawal3212@gmail.com>
2026-08-11 14:26:13 +0530

    Initialize Flask app and add home route

--- api/index.py
+++ api/index.py
@@ -1,6 +1,7 @@
-import sys
-import os
+from flask import Flask
 
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+app = Flask(__name__)
 
-from app import app
+@app.route('/')
+def home():
+    return 'PulseKey is running on Vercel!'
